a83adf67e07c7bd1c996c89cfd86b7cdf417cac0
Clare Matthews <clarematthews@gmail.com>
2014-08-11 16:40:51 +0100

    Initial Commit

---
+++ TrackerMouse/main.m
@@ -0,0 +1,22 @@
+//
+//  main.m
+//  TrackerMouse
+//
+//  Created by Clare Matthews on 11/08/2014.
+//  Copyright (c) 2014 Clare Matthews. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[])
+{
+
+    @autoreleasepool {
+        
+        // insert code here...
+        NSLog(@"Hello, World!");
+        
+    }
+    return 0;
+}
+
